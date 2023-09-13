a6573b3ca9031eacc418fa2a36838449d74ea18c
ludongfang <ldf_5566@163.com>
2023-09-13 15:04:28 +0800

    修改废弃API

--- app/src/main/java/com/we/androiddemo/news_comment/CommentNewsActivity.kt
+++ app/src/main/java/com/we/androiddemo/news_comment/CommentNewsActivity.kt
@@ -2,12 +2,15 @@ package com.we.androiddemo.news_comment
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.we.androiddemo.R
 import com.we.androiddemo.news_comment.logic.impl.FoldReducer
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 /**
@@ -24,23 +27,25 @@ class CommentNewsActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         commentAdapter = CommentAdapter {
-
-            lifecycleScope.launchWhenResumed {
-                val newList = withContext(Dispatchers.IO) {
-                    reduce.invoke(commentAdapter.currentList)
-                }
-                val firstSubmit = commentAdapter.itemCount == 1
-                commentAdapter.submitList(newList) {
-                    // 这里是为了处理submitList后，列表滑动位置不对的问题
-                    if (firstSubmit) {
-                        recyclerView.scrollToPosition(0)
-                    } else if (this@CommentAdapter is FoldReducer) {
-                        val index = commentAdapter.currentList.indexOf(this@CommentAdapter.folding)
-                        recyclerView.scrollToPosition(index)
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                    val newList = withContext(Dispatchers.IO) {
+                        reduce.invoke(commentAdapter.currentList)
+                    }
+                    val firstSubmit = commentAdapter.itemCount == 1
+                    commentAdapter.submitList(newList) {
+                        // 这里是为了处理submitList后，列表滑动位置不对的问题
+                        if (firstSubmit) {
+                            recyclerView.scrollToPosition(0)
+                        } else if (this@CommentAdapter is FoldReducer) {
+                            val index =
+                                commentAdapter.currentList.indexOf(this@CommentAdapter.folding)
+                            recyclerView.scrollToPosition(index)
+                        }
                     }
                 }
-
             }
+
         }
         recyclerView.adapter = commentAdapter
 
